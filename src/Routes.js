b67feedbf72f8d5c78f3f7a8299cc0eaa303bd4f
jasonakon <jasonakon@gmail.com>
2020-03-08 20:37:29 +0800

    Added front end layout and ready for deployment

---
+++ src/Routes.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Switch, Redirect } from 'react-router-dom';
+
+import { RouteWithLayout } from './components';
+import { Main as MainLayout, Minimal as MinimalLayout } from './layouts';
+
+import {
+  Dashboard as DashboardView,
+  ProductList as ProductListView,
+  NotFound as NotFoundView
+} from './views';
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Redirect
+        exact
+        from="/"
+        to="/dashboard"
+      />
+      <RouteWithLayout
+        component={DashboardView}
+        exact
+        layout={MainLayout}
+        path="/dashboard"
+      />
+      <RouteWithLayout
+        component={ProductListView}
+        exact
+        layout={MainLayout}
+        path="/products"
+      />
+      <RouteWithLayout
+        component={NotFoundView}
+        exact
+        layout={MinimalLayout}
+        path="/not-found"
+      />
+      <Redirect to="/not-found" />
+    </Switch>
+  );
+};
+
+export default Routes;
